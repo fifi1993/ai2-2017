5f7a898c50437d99cf3f301a4770578b49ed95f2
fifi1993 <gmp3@onet.eu>
2017-04-26 09:10:00 +0200

    add lab3

---
+++ lab3.py
@@ -0,0 +1,9 @@
+def print_two(a, b):
+    print("Arguments: {0} and {1}".format(a, b))
+
+def keyword_args(a, b=1, c='X', d=None):
+    print("a:", a)
+    print("b:", b)
+    print("c:", c)
+    print("d:", d)
+
